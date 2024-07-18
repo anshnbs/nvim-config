4729ee8f084d4bb11162ba73277330a3cb338353
anshnbs <anthony.shorter@thenbs.com>
2024-07-18 11:33:43 +0100

    Initial

---
+++ init.lua
@@ -0,0 +1,17 @@
+require("config.lazy")
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.mapleader = ' '
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
+vim.cmd.colorscheme("habamax")
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
